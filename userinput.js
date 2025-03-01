6e57e5ba16b104a12faa6d9505fa6007b9175bdd
yebente <yebente@gmail.com>
2025-03-01 10:59:22 +0000

    Sum of User Entered Numbers

---
+++ userinput.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+
+const num1 = parseInt(prompt('Enter the first number: '));
+const num2 = parseInt(prompt('Enter the second number: '));
+
+const sum = num1 + num2;
+
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
